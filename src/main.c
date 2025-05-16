6e2bdb9afa16ca8739de9ecf18f3c8771a38c2bf
Alexey Belov <Alexey.Belov@x-tensive.com>
2025-05-16 17:59:21 +0300

    - cmake: debug build output to plugins directory - plugin export via def file

--- src/main.c
+++ src/main.c
@@ -3,9 +3,35 @@
 #include "openssl/x509.h"
 #include "far/plugin.hpp"
 
-int main()
+HANDLE WINAPI OpenW(const struct OpenInfo *OInfo)
 {
     printf("hi!\n");
-    X509_PUBKEY_eq(NULL, NULL);
-    return 0;
+    return NULL;
 }
+
+void WINAPI ExitFARW(const struct ExitInfo *Info)
+{
+    printf("hi!\n");
+}
+
+void WINAPI GetPluginInfoW(struct PluginInfo *Info)
+{
+    printf("hi!\n");
+}
+
+void WINAPI GetGlobalInfoW(struct GlobalInfo *Info)
+{
+    printf("hi!\n");
+}
+
+void WINAPI SetStartupInfoW(const struct PluginStartupInfo *Info)
+{
+    printf("hi!\n");
+}
+
+// int main()
+// {
+//     printf("hi!\n");
+//     //X509_PUBKEY_eq(NULL, NULL);
+//     return 0;
+// }
